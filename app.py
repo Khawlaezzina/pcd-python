0941de35d069e985b00c2f5de29295af4f8da0f0
khawla <khawla.ezzina@ensi-uma.tn>
2021-05-17 00:48:33 +0100

    my second commit

--- app.py
+++ app.py
@@ -13,78 +13,101 @@ CORS(app)
 conn=pymysql.connect(host='localhost',user='root',password='',database='pcd')
 cursor=conn.cursor()
 query1='select id,rating from freelancer'
-query2='select * from mission where hired=false'
+query2='select id,average_payment,technologies,id_client from missions where hired=0'
 query3='select * from client'
 query4='select name from skills'
 query5='select * from skilled'
 
 #defining a list containing all the name skills presented on the table skills
 def generateSkillsList():
-  cursor.execute(query4)
-  rows=cursor.fetchall()
-  skills=[]
-  for row in rows:
-    skills.append(row[0])
-  return skills
+  try :
+    cursor.execute(query4)
+    rows=cursor.fetchall()
+    skills=[]
+    for row in rows:
+      skills.append(row[0])
+    print(skills)
+    return skills
+  except:
+    print("error dans generateSkillsList")
 
 #defining a matrix containing all non hired missions on each row we find [idMission,skills,ratingClient]
 def generateNonHiredMissionMatrix():
-  skills=generateSkillsList()
-  cursor.execute(query2)
-  missions=cursor.fetchall()
-  VectorMissionHeader=["idMission"]
-  MissionMatrix1=[]
-  for mission in missions :
-    MissionVector=[mission[0]]
-    technologies=mission[6].split("/")  #I should verify the number of column technologies on table mission
-    for skill in skills:
-      VectorMissionHeader.append(skill)
-      if skill in technologies :
-          MissionVector.append(1)
-      else :
-        MissionVector.append(0)
-    cursor.execute('select rating from client where id_client=(select id_client from mission where id_mission=%s)',mission[0]);
-    result=cursor.fetchall()
-    rating=result[0][0]
-    MissionVector.append(rating)
-    MissionMatrix1.append(MissionVector)
-  MissionMatrix=np.array(MissionMatrix1)
-  return MissionMatrix
+  try :
+    skills=generateSkillsList()
+    cursor.execute(query2)
+    missions=cursor.fetchall()
+
+    VectorMissionHeader=["idMission"]
+    MissionMatrix1=[]
+    for mission in missions :
+      MissionVector=[mission[0]]
+      technologies=mission[2].split("/") #I should verify the number of column technologies on table mission
+
+      for skill in skills:
+         VectorMissionHeader.append(skill)
+         if skill in technologies :
+           MissionVector.append(1)
+         else :
+          MissionVector.append(0)
+      query6='select rating from client where id in (select id_client from missions where id=%s)'
+      cursor.execute(query6,mission[3])
+      result=cursor.fetchall()
+      rating=result[0][0]
+      VectorMissionHeader.append('ratingClient')
+      MissionVector.append(rating)
+      MissionMatrix1.append(MissionVector)
+    MissionMatrix=np.array(MissionMatrix1)
+    print(VectorMissionHeader)
+    print(MissionMatrix)
+    return MissionMatrix
+  except:
+    print("error in generateNonHiredMissionMatrix")
 #defining a freelancers Matrix containg on each row [idFreelancer,skills,rating,nbreprojets]
 def generateFreelancersMatrix():
-  skills = generateSkillsList()
-  cursor.execute(query1)
-  freelancers=cursor.fetchall()
-  freelancersMatrix1=[]
-  Freelancerheaders = ["idfreelancer"]
-  for skill in skills :
-    Freelancerheaders.append(skill)
-  Freelancerheaders.append("rating")
-  Freelancerheaders.append("NombreProjets")
-  for freelancer in freelancers:
-    query = 'select skills.name from skills where id_skill in (select id_skill from skilled where freelancer_id=%s',freelancer[0]
-    cursor.execute(query)
-    result1 = cursor.fetchall()
-    freelancerSkills=[]
-    for result in result1 :
-      freelancerSkills.append(result[0])
-    freelancerList = [freelancer[0]]
-    for skill in skills:
-      if (skill in freelancerSkills):
-        freelancerList.append(1)
-      else:
-        freelancerList.append(0)
+  try:
+    skills = generateSkillsList()
+    cursor.execute(query1)
+    freelancers=cursor.fetchall()
 
-    freelancerList.append(freelancer[1])
-    embeddedquery='select count(id_mission) from mission where completed=true and id_freelancer=%s',freelancer[0]
-    cursor.execute(embeddedquery)
-    result2=cursor.fetchall()
-    nombreProjets=result2[0][0]
+    freelancersMatrix1=[]
+    Freelancerheaders = ["idfreelancer"]
+    for skill in skills :
+      Freelancerheaders.append(skill)
+    Freelancerheaders.append("rating")
+    Freelancerheaders.append("NombreProjets")
 
-    freelancerList.append(nombreProjets)
-  freelancersMatrix1.append(freelancerList)
-  freelancersMatrix=np.array(freelancersMatrix1)
-  return freelancersMatrix
+    for freelancer in freelancers:
+      query = 'select skills.name from skills where id_skill in (select skill_id from skilled where freelancer_id=%s)'
+      cursor.execute(query,freelancer[0])
+      result1 = cursor.fetchall()
+
+      freelancerSkills=[]
+      for result in result1 :
+        freelancerSkills.append(result[0])
+
+      freelancerList = [freelancer[0]]
+      for skill in skills:
+        if (skill in freelancerSkills):
+          freelancerList.append(1)
+        else:
+          freelancerList.append(0)
+
+      freelancerList.append(freelancer[1])
+
+      embeddedquery='select count(id) from missions where completed=true and id_freelancer=%s'
+      cursor.execute(embeddedquery,freelancer[0])
+      result2=cursor.fetchall()
+
+      nombreProjets=result2[0][0]
+
+      freelancerList.append(nombreProjets)
+      freelancersMatrix1.append(freelancerList)
+    print(Freelancerheaders)
+    print(freelancersMatrix1)
+    return freelancersMatrix1
+  except:
+    print("Error in generate Freelancers Matrix")
 freelancers= {
   "data": [
     {
@@ -100,9 +123,14 @@ freelancers= {
   ]
 }
 
-def calculRatingNombreProjetFactor(rating,pourcentageRating,nombreProjet,pourcentageNombreProjet):
-  return (pourcentageRating/100)*rating+(pourcentageNombreProjet/100)*nombreProjet
+def calculRatingNombreProjetFactor(rating,nombreProjet,similarity):
+  return 0.37*rating+0.3*nombreProjet+0.33*similarity
+def calculOfferId(Matrix):
+  OffersId=[]
+  for line in Matrix:
 
+    OffersId.append(int(line[0]))
+  return OffersId
 @app.route('/',methods=['GET'])
 def hello_world():
     return 'Hello World!'
@@ -112,84 +140,131 @@ def freelancersReport():
   global freelancers
   return jsonify([freelancers])
 #recommand offers to a particular freelancer
-@app.route('/recommandOffer/<idfreelancer>')
+@app.route('/recommandOffer/<idfreelancer>',methods=['GET','POST'])
 def recommandOffers(idfreelancer):
-  query='select skills.name from skills where id_skill in (select id_skill from skilled where freelancer_id=%s', idfreelancer
-  cursor.execute(query)
-  result1 = cursor.fetchall()
-  freelancerSkills = []
-  headers = ['idfreelancer']
-  skills = generateSkillsList()
-  for skill in skills :
-    headers.append(skill)
-  for result in result1:
-    freelancerSkills.append(result[0])
+  try :
+    skills = generateSkillsList()
 
-  freelancerList=[idfreelancer]
-  OffersId=[]
-  nombreSkills=0  #to represent the nombre of skills that a particular freelancer have
-  for skill in skills:
-    if(skill in freelancerSkills) :
-      freelancerList.append(1)
-      nombreSkills += 1
-    else :
-      freelancerList.append(0)
+    query='select skills.name from skills where id_skill in (select skill_id from skilled where freelancer_id=%s)'
+    cursor.execute(query,idfreelancer)
+    result1 = cursor.fetchall()
+    print(result1)
+    freelancerSkills = []
+    headers = ['idfreelancer']
 
-  freelancerVector=np.array(freelancerList)
-  SimilarityList=[]
-  MissionMatrix=generateNonHiredMissionMatrix()
-  for mission in MissionMatrix :
-    shape=np.shape(mission)
-    result=np.dot(freelancerVector[1:],mission[:][1:shape-2])
-    SimilarityList.append(result[0][0])
-  SelectedMissions=[]
-  j=0
-  for mission in MissionMatrix:
-    if SimilarityList[j]>=nombreSkills*2/3 :
-        SelectedMissions.append(mission)
-    j+=1
-  SortedSelectedMission=sorted(SelectedMissions,lambda mission:mission[shape(MissionMatrix)[1]-1],reverse=True)
-  for item in SortedSelectedMission:
-    OffersId.append(item[0])
-  return OffersId
+    for skill in skills :
+      headers.append(skill)
+    for result in result1:
+      freelancerSkills.append(result[0])
+
+    freelancerList=[idfreelancer]
+    nombreSkills=0  #to represent the nombre of skills that a particular freelancer have
+    for skill in skills:
+      if(skill in freelancerSkills) :
+        freelancerList.append(1)
+        nombreSkills += 1
+      else :
+        freelancerList.append(0)
+    freelancerVector=np.array(freelancerList)
+    print(headers)
+    print(freelancerVector)
+    newFreelancerVector=[]
+    for i in freelancerVector:
+      newFreelancerVector.append(int(i))
+    SimilarityList=[]
+    MissionMatrix=generateNonHiredMissionMatrix()
+    for mission in MissionMatrix :
+      shape=np.shape(mission)
+      nombreColonne=shape[0]
+
+      result=np.dot(newFreelancerVector[1:],mission[1:nombreColonne-1])
+
+      SimilarityList.append(result)
+
+    SelectedMissions=[]
+    j=0
+    for mission in MissionMatrix:
+      if SimilarityList[j]>=1.0 :
+        SelectedMissions.append(mission.tolist())
+      j+=1
+    print(SelectedMissions)
+
+    index=len(SelectedMissions[0])-1
+
+    SortedSelectedMission=sorted(SelectedMissions,key=lambda mission: mission[index],reverse=True)
+    print(SortedSelectedMission)
+    OffersId=calculOfferId(SortedSelectedMission)
+    print(OffersId)
+
+    return jsonify(OffersId)
+  except:
+    print("Error in recommand offers for a freelancer")
+    return 'Error'
 #recommmand freelancers to a particular mission
-@app.route('/recommandfreelancer/<idMission>')
+@app.route('/recommandfreelancer/<idMission>',methods=['GET','POST'])
 def recommandfreelancers(idMission):
-  query='select technologies from mission where idMission=%s',idMission
-  cursor.execute(query)
-  technologie = cursor.fetchall()
-  technologies = technologie[0][0].split("/")
-  nombreSkillsDemanded=np.shape(technologies)
-  freelancersId=[]
-  MissionHeader = ["idMission"]
-  MissionList=[idMission]
-  for skill in skills:
+  try:
+    query='select technologies from missions where id=%s'
+    cursor.execute(query,idMission)
+    technologie = cursor.fetchall()
+    print(technologie)
+    skills = generateSkillsList()
+    print(skills)
+    technologies = technologie[0][0].split("/")
+    print(technologies)
+    numbreSkillsDemanded=np.shape(technologies)[0]
+    print(numbreSkillsDemanded)
+    MissionHeader = ["idMission"]
+    MissionList=[int(idMission)]
+    for skill in skills:
       MissionHeader.append(skill)
       if skill in technologies:
-        MissionList.append(1)
+        MissionList.append(int(1))
       else:
-        MissionList.append(0)
-  MissionInfoVector=np.array(MissionList)
-  SimilarityList=[]
-  for freelancer in freelancersMatrix :
-    shape=np.shape(freelancer)
-    result=np.dot(MissionInfoVector[1:],freelancer[:][1:shape-2])
-    SimilarityList.append(result[0][0])
-  SelectedFreelancers=[]
-  j=0
-  for freelancer in freelancersMatrix:
-    if SimilarityList[j]>=nombreSkillsDemanded :
-        SelectedFreelancers.append(freelancer)
-    j+=1
-  #We will replace the two last columns (rating,nbPorject) for each row in SelectedFreelancers by the factor calculated and put the result in a new matrix
-  NewFreelancersMatrix=[]
-  for freelancer in SelectedFreelancers:
-      freelancerInfo=freelancer[0:shape(freelancer)-2]
-      calculatedfactor=calculRatingNombreProjetFactor(freelancer[-2],freelancer[-1])
+        MissionList.append(int(0))
+    MissionInfoVector=np.array(MissionList)
+    print(MissionHeader)
+    print(MissionInfoVector)
+    SimilarityList=[]
+    freelancersMatrix=np.array(generateFreelancersMatrix())
+    print(freelancersMatrix)
+    for freelancer in freelancersMatrix :
+
+      shape=np.shape(freelancer)[0]-2
+
+      result=np.dot(MissionInfoVector[1:],freelancer[1:shape])
+
+      SimilarityList.append(int(result))
+    print(freelancersMatrix)
+    i=0
+  #We will replace the two last columns (rating,nbPorject,SimilarityList) for each row in SelectedFreelancers by the factor calculated and put the result in a new matrix
+    NewFreelancersMatrix=[]
+    NewHeaders=['idFreelancer']
+    for skill in skills :
+      NewHeaders.append(skill)
+    NewHeaders.append('CalculatedFactor')
+    for freelancer in freelancersMatrix:
+
+
+      print(len(freelancer))
+      freelancerInfo=freelancer[0:len(freelancer)-2].tolist()
+      print(freelancerInfo)
+      queryLanguages='select count(id_language) from speak where id_freelancer=%s'
+      cursor.execute(queryLanguages,int(freelancerInfo[0]))
+      numberOfLanguagesSpeaked=cursor.fetchall()[0][0]
+      calculatedfactor=calculRatingNombreProjetFactor(freelancer[-2],freelancer[-1],SimilarityList[i])
       freelancerInfo.append(calculatedfactor)
       NewFreelancersMatrix.append(freelancerInfo)
-  SortedSelectedfreelancers = sorted(NewFreelancersMatrix, lambda factor: freelancer[shape(NewFreelancersMatrix)[1]-1], reverse=True)
-  freelancersId=[freelancer[0] for freelancer in SortedSelectedfreelancers]
-  return freelancersId
+      i+=1
+    print(NewHeaders)
+    print(NewFreelancersMatrix)
+    index=len(NewFreelancersMatrix[0])-1
+    SortedSelectedfreelancers = sorted(NewFreelancersMatrix,key=lambda freelancer: freelancer[index], reverse=True)
+    freelancersId=[int(freelancer[0]) for freelancer in SortedSelectedfreelancers]
+    print(freelancersId)
+    return jsonify(freelancersId)
+  except:
+    print("Error on recommand freealncers for an offer")
+    return 'Error'
 if __name__ == '__main__':
     app.run()
